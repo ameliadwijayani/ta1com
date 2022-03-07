26f274c7b214b27bad1ff016be3c27e4cbc4347c
ameliadwijayani <57041004+ameliadwijayani@users.noreply.github.com>
2022-03-07 15:45:27 +0700

    c

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-sharp`,
+    // `gatsby-plugin-sharp`,
     `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
